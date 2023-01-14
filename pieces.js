547a363e7ac50dce2b8b47319f67030e552f0152
Ornella B <borges.ornella@gmail.com>
2023-01-14 21:07:04 +0100

    modif piece

--- pieces.js
+++ pieces.js
@@ -1,4 +1,4 @@
-const reponse = await fetch('pieces-autos.json');
+/* const reponse = await fetch('pieces-autos.json');
 console.log(reponse)
 const pieces = await reponse.json();
 console.log(pieces)
@@ -18,6 +18,30 @@ sectionFiche.appendChild(imageElement);
 sectionFiche.appendChild(nomElement);
 sectionFiche.appendChild(prixElement);
 sectionFiche.appendChild(categorieElement);
+
+ */
+const reponse = await fetch ('pieces-auto.json');
+const piece = await reponse.json();
+
+const article = pieces[0];
+const imageElement = document.createElement("img")
+imageElement.src = article.image;
+const nomElement = document.createElement("h2")
+nomElement.innerText = article.nom; 
+const prixElement = document.createElement("p")
+prixElement.innerText = `Prix: ${article.prix} €`;
+const categorieElement = document.createElement("p");
+categorieElement.innerText = article.categorie; 
+
+const sectionFiches = document.querySelector(".fiches");
+sectionFiches = appendChild(imageElement);
+sectionFiches = appendChild(nomElement);
+sectionFiches = appendChild(prixElement);
+sectionFiches = appendChild(categorieElement);
+
+
+
+
 
 
 
