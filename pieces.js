49224fc9b8fc000fc77b6ea4f4aa6695f49365f2
Ornella B <borges.ornella@gmail.com>
2023-01-26 19:46:16 +0100

    suite du cours

--- pieces.js
+++ pieces.js
@@ -59,3 +59,21 @@ for (let i = 0; i < pieces.length; i++) {
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(stockElement);
 }
+
+console.log(pieces)
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", function() {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function(a, b){
+        return a.prix - b.prix
+    })
+    console.log(piecesOrdonnees);
+});
+
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+boutonFiltrer.addEventListener("click", function() {
+    const piecesFiltrees = pieces.filter(function(piece){
+        return piece.prix <= 35;
+    });
+    console.log(piecesFiltrees)
+});
